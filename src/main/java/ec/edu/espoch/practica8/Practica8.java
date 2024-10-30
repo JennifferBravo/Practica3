34c4c632d4d7a4cc4a0d3b797863803a6f565ad4
jenniffermuima. <jenniffermuima2001@gmail.com>
2024-10-30 12:53:26 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/practica8/Practica8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practica8;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Practica8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
